3bd1dd63632e3ff1f5d0bb49417a4f3fd5bd9396
Valeria Santos <valeria.redesociais@gmail.com>
2025-03-19 05:54:49 -0300

    chore: polarity

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from textblob import TextBlob
 
-app = Flask('meu_app')
+app = Flask('__name__')
 
 @app.route('/')
 def home():
     return "Minha primeira API."
 
-app.run()
+@app.route('/sentimento/<frase>')
+def sentimento(frase):
+    tb = TextBlob(frase)
+    try:
+        tb_en = tb.translate(to='en')
+    except Exception as e:
+        tb_en = tb
+    polaridade = tb_en.sentiment.polarity
+    return "polaridade: {}".format(polaridade)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
