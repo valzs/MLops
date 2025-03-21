606e4f5bce1186447bd86527da26c2bac38db990
Valeria Santos <valeria.redesociais@gmail.com>
2025-03-21 07:19:57 -0300

    chore: add file and new route

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from flask import Flask
 from textblob import TextBlob
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+df = pd.read_csv('casas.csv')
+
+colunas= ['tamanho','preco']
+df = df[colunas]
+
+x = df.drop('preco', axis=1)
+y = df['preco']
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.3, random_state=42)
+
+modelo = LinearRegression()
+modelo.fit(x_train, y_train)
 
 app = Flask('__name__')
 
@@ -17,6 +34,11 @@ def sentimento(frase):
     polaridade = tb_en.sentiment.polarity
     return "polaridade: {}".format(polaridade)
 
+@app.route('/cotacao/<int:tamanho>')
+def cotacao(tamanho):
+    preco = modelo.predict([[tamanho]])
+    return "O preço da casa de tamanho {} é de {}".format(tamanho, preco)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+
+app.run(debug=True)
