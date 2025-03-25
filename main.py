7e420b42044b2ac2404b1ed80559104dc55aac44
Valeria Santos <valeria.redesociais@gmail.com>
2025-03-25 19:48:49 -0300

    chore: add basic auth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_basicauth import BasicAuth
 from textblob import TextBlob
 from sklearn.linear_model import LinearRegression
 import pickle 
@@ -7,12 +8,17 @@ colunas = ['tamanho', 'ano', 'garagem']
 modelo= pickle.load(open('modelo.sav', 'rb'))
 
 app = Flask('__name__')
+app.config['BASIC_AUTH_USERNAME'] = 'admin'
+app.config['BASIC_AUTH_PASSWORD'] = 'github'
+
+basic_auth = BasicAuth(app)
 
 @app.route('/')
 def home():
     return "Minha primeira API."
 
 @app.route('/sentimento/<frase>')
+@basic_auth.required
 def sentimento(frase):
     tb = TextBlob(frase)
     try:
