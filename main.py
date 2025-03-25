aef5cec0903afe27e96178ae416d9404407bec4e
Valeria Santos <valeria.redesociais@gmail.com>
2025-03-25 19:30:02 -0300

    refactor: serialização Pickle

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 from flask import Flask, request, jsonify
 from textblob import TextBlob
-import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+import pickle 
 
-df = pd.read_csv('casas.csv')
 colunas = ['tamanho', 'ano', 'garagem']
-x = df.drop('preco', axis=1)
-y = df['preco']
-
-x_train, x_test, y_train, y_test = train_test_split(
-    x, y, test_size=0.3, random_state=42)
-
-modelo = LinearRegression()
-modelo.fit(x_train, y_train)
+modelo= pickle.load(open('modelo.sav', 'rb'))
 
 app = Flask('__name__')
 
